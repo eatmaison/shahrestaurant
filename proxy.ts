f71dafafc5ca165bdd90eb6b7570079b5bbe35b6
eatmaison <cv.komanda@gmail.com>
2026-08-02 19:41:04 +0100

    Updateeatto

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const legacyHosts = new Set(["order.thetandoorcompany.nl"]);
+const primaryHosts = new Set(["thetandoorcompany.nl", "www.thetandoorcompany.nl"]);
 
 export function proxy(request: NextRequest) {
   const host = request.headers.get("host")?.split(":")[0].toLowerCase();
 
-  if (host && legacyHosts.has(host)) {
+  if (host?.endsWith(".thetandoorcompany.nl") && !primaryHosts.has(host)) {
     return new NextResponse("Not Found", {
       status: 404,
       headers: { "X-Robots-Tag": "noindex" },
